9c0087ed2eb78b58b4eb5e2a6169c4f270818926
Shark-44 <joanny.bernardeau@gmail.com>
2024-02-21 12:21:32 +0100

    quete des fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    private const CATEGORIES =[
+        'Action',
+        'Aventure',
+        'Animation',
+        'Fantastique',
+        'Horreur',
+        'Science-Fiction',
+        'Comédie',
+        'Drame'
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $categoryName){
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+            $this->addReference('category_' . $categoryName, $category);
+        }
+
+        $manager->flush();
+    }
+}
