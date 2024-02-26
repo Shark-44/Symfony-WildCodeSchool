68b0bd479fd1487abf0248243a68ce0abe55135e
Shark-44 <joanny.bernardeau@gmail.com>
2024-02-26 15:40:26 +0100

    j'ai bien mes consignes mais category_id dans l'entity program est la deuxieme column???

--- migrations/Version20240225131346.php
+++ migrations/Version20240225131346.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240221090441 extends AbstractMigration
+final class Version20240225131346 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,16 @@ final class Version20240221090441 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE program ADD title VARCHAR(255) NOT NULL, DROP program_id, CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE synopsis synopsis VARCHAR(255) NOT NULL, ADD PRIMARY KEY (id)');
+        $this->addSql('ALTER TABLE program ADD category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED778412469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_92ED778412469DE2 ON program (category_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE program MODIFY id INT NOT NULL');
-        $this->addSql('DROP INDEX `primary` ON program');
-        $this->addSql('ALTER TABLE program ADD program_id INT NOT NULL, DROP title, CHANGE id id INT NOT NULL, CHANGE sysnopsis sysnopsis LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED778412469DE2');
+        $this->addSql('DROP INDEX IDX_92ED778412469DE2 ON program');
+        $this->addSql('ALTER TABLE program DROP category_id');
     }
 }
